c803eb089641eb417443d96d00ac1df7242a9d64
Akarshit <akarshit.wal@mailchimp.com>
2022-09-13 02:46:38 -0400

    chore: dep

--- brain/api.js
+++ brain/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 import TokenService from '#services/token';
-import { dev } from 'config';
+import { server } from 'config';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3000/api/v1',
+  baseURL: server,
   timeout: 2000,
 });
 
--- config.js
+++ config.js
@@ -1,1 +1,4 @@
 export const dev = process.env !== 'production';
+export const server = dev
+  ? 'http://localhost:3000/api/v1'
+  : 'https://reservenature/api/v1';
